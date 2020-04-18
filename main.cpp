0b7ed61556ca723f008d35b0875be2bd70b7467e
saber king <saberking321@gmail.com>
2020-04-18 16:00:19 +0100

    attroll

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ struct Item {
         type.assign(_type);
     }
 };
+    sf::RenderWindow window(sf::VideoMode(1200, 700), "SFML works!");
 
 struct Being {
     int Str;
@@ -22,8 +23,11 @@ struct Being {
     int cDex;
     int cInt;
     int cVit;
+    int com, ban, mag;
+    int sta;
     list<Item> stuff;
-    Being(){
+    string name;
+    Being(string _name){
         Str=1;
         Dex=1;
         Int=1;
@@ -32,12 +36,47 @@ struct Being {
         cDex=1;
         cInt=1;
         cVit=1;
+        com=0;
+        ban=0;
+        mag=0;
+        name=_name;
+        sta=Vit*5;
     }
 };
+Being p1("p1"), p2("p2"), p3("p3"), p4("p4"), goblin("goblin");
+Being chars[4]={p1,p2,p3,p4};
+sf::Sprite sprite, ch1, ch2, ch3, ch4;
+sf::Texture texture, ch1t, ch2t, ch3t, ch4t;
+
 void setup(){
+    srand(time(NULL));
+        font.loadFromFile("fonts/thestrong.ttf");
 
+    texture.loadFromFile("Gob.png");
+sprite.setTexture(texture);
+    ch1t.loadFromFile("pics/ch1.png");
+ch1.setTexture(ch1t);
+ch1.setPosition(sf::Vector2f(900.f, 100.f));
+ch1.setScale(sf::Vector2f(0.75f, 0.75f));
+    ch2t.loadFromFile("pics/ch2.png");
+ch2.setTexture(ch2t);
+ch2.setPosition(sf::Vector2f(900.f, 250.f));
+ch2.setScale(sf::Vector2f(0.75f, 0.75f));
+    ch3t.loadFromFile("pics/ch3.png");
+ch3.setTexture(ch3t);
+ch3.setPosition(sf::Vector2f(900.f, 400.f));
+ch3.setScale(sf::Vector2f(0.75f, 0.75f));
+
+    ch4t.loadFromFile("pics/ch4.png");
+ch4.setTexture(ch4t);
+ch4.setPosition(sf::Vector2f(900.f, 550.f));
+ch4.setScale(sf::Vector2f(0.75f, 0.75f));
 }
-
+int attackRoll(Being &a, Being &d){
+    int att=rand()%6+a.com+a.Dex;
+    int def=rand()%6+d.com+d.Dex;
+    return att-def;
+}
 struct StatementBox{
     string statements[7];
     int noOfLines;
@@ -80,50 +119,45 @@ addStatement("You are attacked by an unarmed goblin!");
     };
     sf::Text text;
 };
+bool checkBalance(Being &a){
+    return rand()%6<a.Dex;
+}
     StatementBox msg;
 int noOfPlayers=5;
 int currentPlayer=1;
 void nextCharacter(){
     currentPlayer++;
     if(currentPlayer>noOfPlayers)currentPlayer=1;
-
-    msg.addStatement(currentPlayer<=4?"Player " +to_string(currentPlayer)+" attacks. Missed!" :"Goblin attacks. Missed!");
+    Being *attacker, *defender;
+    if(currentPlayer<5){
+        attacker=&chars[currentPlayer-1];
+        defender=&goblin;
+    }else{
+        attacker=&goblin;
+        defender=&chars[rand()%4];
+    }
+    string message=attacker->name+" attacks "+defender->name+".";
+    int dam=attackRoll(*attacker, *defender);
+    if(dam<1){
+        message+=" Missed!";
+        if(!checkBalance(*attacker))message+=" "+attacker->name+" falls over!";
+    }else{
+        dam*=attacker->Str;
+        defender->sta-=dam;
+        message+=" "+to_string(dam)+" stamina lost.";
+    }
+    msg.addStatement(message);
 }
 
 int main()
 {
-    font.loadFromFile("fonts/thestrong.ttf");
+setup();
 
-sf::Texture texture, ch1t, ch2t, ch3t, ch4t;
-    texture.loadFromFile("Gob.png");
-    sf::Sprite sprite;
-sprite.setTexture(texture);
-    ch1t.loadFromFile("pics/ch1.png");
-    sf::Sprite ch1;
-ch1.setTexture(ch1t);
-ch1.setPosition(sf::Vector2f(900.f, 100.f));
-ch1.setScale(sf::Vector2f(0.75f, 0.75f));
-    ch2t.loadFromFile("pics/ch2.png");
-    sf::Sprite ch2;
-ch2.setTexture(ch2t);
-ch2.setPosition(sf::Vector2f(900.f, 250.f));
-ch2.setScale(sf::Vector2f(0.75f, 0.75f));
-    ch3t.loadFromFile("pics/ch3.png");
-    sf::Sprite ch3;
-ch3.setTexture(ch3t);
-ch3.setPosition(sf::Vector2f(900.f, 400.f));
-ch3.setScale(sf::Vector2f(0.75f, 0.75f));
-
-    ch4t.loadFromFile("pics/ch4.png");
-    sf::Sprite ch4;
-ch4.setTexture(ch4t);
-ch4.setPosition(sf::Vector2f(900.f, 550.f));
-ch4.setScale(sf::Vector2f(0.75f, 0.75f));
-
-    sf::RenderWindow window(sf::VideoMode(1200, 700), "SFML works!");
 
 sf::Clock clock;
 sf::Time elapsed;
+        sf::Event event;
+
 window.setFramerateLimit(20);
     while (window.isOpen())
     {
@@ -132,7 +166,6 @@ window.setFramerateLimit(20);
             clock.restart();
             nextCharacter();
         }
-        sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
